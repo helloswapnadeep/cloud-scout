5a06ff9e6d042b1d518cc340c599ca8c83e7f832
helloswapnadeep <helloswapnadeep@outlook.com>
2024-12-29 13:30:22 +0530

    Preloader

---
+++ components/scripts.js
@@ -0,0 +1,17 @@
+// ! PRE LOADER START
+
+window.onload = function () {
+  var preloader = document.getElementsByClassName("preloader")[0];
+  setTimeout(function () {
+    preloader.style.display = "none";
+  }, 500);
+};
+
+// ! PRE LOADER END
+// ! HIDDEN MESSAGE START
+
+const scout =
+  "Hello, traveler of the skies and seeker of forecasts! 🌈 Whether you're chasing sunny days, preparing for the rain, or marveling at the stars above, we’ve got you covered 🌦️. So, grab a cup of coffee ☕, enjoy the vibrant visuals, and let us guide you through nature's wonders, one forecast at a time. Your adventure under the skies starts here! 🌍";
+console.log(scout);
+
+// ! HIDDEN MESSAGE END
